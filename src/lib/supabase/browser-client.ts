f7d9e0478d7eb23fe532a284489d6ca2246cc81c
Anthonymejia0 <mejia.anthony96@gmail.com>
2026-04-29 13:33:49 -0700

    include supabase utils

---
+++ src/lib/supabase/browser-client.ts
@@ -0,0 +1,25 @@
+'use client';
+
+import { createBrowserClient } from '@supabase/ssr';
+import { SupabaseClient } from '@supabase/supabase-js';
+import { Database } from '../../../database.types';
+
+let client: SupabaseClient<Database> | null = null;
+
+export function getSupabaseBrowserClient(): SupabaseClient<Database> {
+  if (client) return client;
+
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabasePublishableKey =
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+  if (!supabaseUrl || !supabasePublishableKey) {
+    throw new Error(
+      'Missing NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY',
+    );
+  }
+
+  client = createBrowserClient<Database>(supabaseUrl, supabasePublishableKey);
+
+  return client;
+}
